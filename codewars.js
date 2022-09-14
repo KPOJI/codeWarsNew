f2de021066a0e824ed84401de68cac06f28e9692
Dmitriy <g.o.d@list.ru>
2022-09-14 20:12:11 +0300

    add firstNonRepeatingLetter

--- codewars.js
+++ codewars.js
@@ -1,4 +1,6 @@
-function pigIt(str){
+//Simple Pig Latin
+
+/* function pigIt(str){
 	let strArr = str.split(' ')
 	let strEnd = []
 	for (let i = 0; i < strArr.length; i++) {
@@ -15,4 +17,36 @@ function pigIt(str){
 console.log(strEnd.join(' '))
 return strEnd.join(' ')
 }
-pigIt('hello world !')
+pigIt('hello world !') */
+
+
+
+//firstNonRepeatingLetter
+let a = 'sTreSS'
+function firstNonRepeatingLetter(s) {
+  console.log(s)	
+	let arr = s.toLowerCase().split('')
+console.log(arr)	
+	let uniqueArr =[]
+	let aaa = []
+	let i=0;
+	console.log(arr.slice(1))
+	while (i<arr.length){
+		let arr1 = arr.slice(i+1)
+		!arr1.includes(arr[i])? uniqueArr.push(arr[i]): aaa.push(arr[i])
+		i++
+	}
+	console.log(uniqueArr)
+	console.log(aaa);
+	let itog = []
+	for (let el of uniqueArr) {aaa.includes(el)? el : itog.push(el) }
+	console.log(itog);
+  itog.length===0? itog = ''  : itog = s[arr.findIndex(x=>x===itog[0])]
+  console.log(itog)
+	return itog
+}
+firstNonRepeatingLetter(a)
+let c = a.toLowerCase()
+console.log(c.indexOf(''))
+let b = [...c].findIndex((n, i, a) => a.indexOf(n) === a.lastIndexOf(n)) || ''
+console.log(a[b]||'')
