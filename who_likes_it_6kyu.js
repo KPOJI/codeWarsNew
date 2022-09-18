a67bd745da98df8111de9539cd79e30dd3bf7dd6
Dmitriy <99410092+KPOJI@users.noreply.github.com>
2022-09-18 10:31:48 +0300

    Delete who_likes_it_6kyu.js

--- who_likes_it_6kyu.js
+++
@@ -1,8 +0,0 @@
-function likes(names) {
-	let b
-	names.length<=1? b = ' likes this' : b = ' like this'
-    names.length===0? `no one${b}` : names.length===1? `${names+b}` :names.reduce((x,y)=>x+','+y+' and '+`${b}`)
-
-	
-}
-console.log()
